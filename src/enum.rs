8d734051e13809d7f51a1d218939397445213001
Peter MIKOLA <mikopet@gmail.com>
2024-01-28 15:29:09 +0100

    :sparkles: add derive macros to `Digit`

--- src/enum.rs
+++ src/enum.rs
@@ -1,5 +1,5 @@
 #[non_exhaustive]
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Digit {
     Zero = 0,
     One = 1,
@@ -29,5 +29,37 @@ mod tests {
         assert_eq!(7, Digit::Seven as i32);
         assert_eq!(8, Digit::Eight as i64);
         assert_eq!(9, Digit::Nine as i128);
+    }
+
+    #[test]
+    fn eq() {
+        assert_eq!(Digit::One, Digit::One);
+        assert_ne!(Digit::Two, Digit::Three);
+    }
+
+    #[test]
+    fn comp() {
+        assert_eq!(Digit::One <= Digit::One, true);
+        assert_eq!(Digit::Two < Digit::Three, true);
+        assert_eq!(Digit::Four > Digit::Five, false);
+    }
+
+    #[test]
+    fn ord_sort() {
+        let mut a = [Digit::Four, Digit::Zero, Digit::Nine];
+        a.sort();
+
+        assert_eq!(a, [Digit::Zero, Digit::Four, Digit::Nine]);
+    }
+
+    #[test]
+    fn ord_pq() {
+        use std::collections::BinaryHeap;
+
+        let mut heap = BinaryHeap::new();
+        heap.push(Digit::One);
+        heap.push(Digit::Eight);
+        heap.push(Digit::Six);
+        assert_eq!(heap.pop().unwrap(), Digit::Eight);
     }
 }
